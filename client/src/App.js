80c8d3916b1964e1f213a06f2724914964bee9f8
Nathan Montelli <nathanmontelli@gmail.com>
2022-01-25 14:05:58 -0800

    added user register and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './pages/Login'
+import Register from './pages/Register'
+// import Home from './pages/Home'
 const App = () => {
   return (
-    <>
-    
-    </>
+    <Router>
+      <Routes>
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        {/* <Route path='/' element={<Home />} /> */}
+      </Routes>
+    </Router>
   )
 }
 
-export default
+export default App
--- client/src/components/RegisterForm/index.js
+++ client/src/components/RegisterForm/index.js
@@ -1,1 +1,1 @@
-export { default } from '.RegisterForm.js'
+export { default } from './RegisterForm.js'
