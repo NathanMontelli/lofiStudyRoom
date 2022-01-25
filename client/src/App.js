23b7e869e6d1013c69912d5439d693802c924a8a
kelleyindahouse <91229799+kelleyindahouse@users.noreply.github.com>
2022-01-25 17:44:11 -0500

    text editor stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
+import TextEditor from "../src/components/TextEditor";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom'
+import { v4 as uuidV4 } from 'uuid'
+
 const App = () => {
   return (
     <>
-      <h1>hello</h1>
+      <a href='/notes'>notes</a>
+      <Router>
+        <Routes>
+          <Route path='/notes' element={<Navigate to={`/documents/${uuidV4()}`} />} />
+          <Route path='/documents/:id' element={<TextEditor />} />
+        </Routes>
+      </Router>
     </>
   )
 }
--- server.js
+++ server.js
@@ -6,7 +6,47 @@ const passport = require('passport')
 const { Strategy: LocalStrategy } = require('passport-local')
 const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt')
 const mongoose = require('mongoose')
-const Document = require('./Document')
+const Document = require('./models/Document')
+
+mongoose.connect('mongodb://localhost:27017/google-docs-clone');
+
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+})
+
+const defaultValue = ''
+
+// listening for text changes
+io.on("connection", socket => {
+  socket.on('get-document', async documentId => {
+    // capturing function to find document by Id
+    const document = await findOrCreateDocument(documentId)
+    // putting socket into a 'room' based on documentId and everyone with this socket can talk to one another
+    socket.join(documentId)
+    // send out data from matching document
+    socket.emit('load-document', document.data)
+
+    socket.on('send-changes', delta => {
+      // broadcasts to everyone but 'us' that there are changes and 'delta' are those changes
+      socket.broadcast.to(documentId).emit('receive-changes', delta)
+    })
+    // updating saved data on documents
+    socket.on('save-document', async data => {
+      await Document.findByIdAndUpdate(documentId, { data })
+    })
+  })
+})
+
+async function findOrCreateDocument(id) {
+  if (id == null) return
+
+  const document = await Document.findById(id)
+  if (document) return document
+  return await Document.create({ _id: id, data: defaultValue })
+}
 
 const app = express()
 const { User } = require('./models')
