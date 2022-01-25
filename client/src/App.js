dde28ff625551732d2eea94473ad79defed35f8b
email <abe.solis2015@gmail.com>
2022-01-25 15:56:45 -0800

    Navbar Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './components/Navigation'
+
 const App = () => {
   return (
     <>
-    
+    <Navigation />
     </>
   )
 }
