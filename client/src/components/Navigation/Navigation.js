51b3f58249924fdd39be6d44352a150d72127f8a
email <abe.solis2015@gmail.com>
2022-01-25 17:04:51 -0800

    Navbar Fonts and Animations

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,42 +1,53 @@
 import React from 'react';
+import { useEffect } from 'react';
 import './Navigation.css';
-import { Nav, Navbar, NavDropdown, Offcanvas, Button, Form,FormControl, Container} from "react-bootstrap";
-
+import { Nav, Navbar, NavDropdown, Offcanvas, Button, Form, FormControl, Container } from "react-bootstrap";
+import { gsap } from "gsap";
 
 
 
 const Navigation = () => {
 
+  const tl = gsap.timeline({ defaults: { ease: 'power1.out' } })
+
+  useEffect(() => {
+
+
+
+    //Specify a starting and ending point in this case the opacity
+    tl.fromTo('.logo', { opacity: 0 }, { opacity: 1, duration: 3 })
+    tl.fromTo('.hamburger', { opacity: 0 }, { opacity: 1, duration: 3 })
+
+  })
+
   return (
     <Navbar bg="" expand={false}>
       <Container fluid>
-        <Navbar.Brand  className = "logo"href="#">Lofi Study Room</Navbar.Brand>
+        <Navbar.Brand className="logo" href="#">Lofi Study Room</Navbar.Brand>
         <Navbar.Toggle aria-controls="offcanvasNavbar" />
         <Navbar.Offcanvas
           id="offcanvasNavbar"
           aria-labelledby="offcanvasNavbarLabel"
           placement="end"
         >
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+          <Offcanvas.Header closeButton >
+            <Offcanvas.Title
+              className="nav-menu" id="offcanvasNavbarLabel">Lofi Menu</Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
             <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
+              <Nav.Link className="nav-links" href="#action2">Profile</Nav.Link>
+              <Nav.Link className="nav-links" href="#action2">Background</Nav.Link>
+              <NavDropdown className="nav-station" title="Stations" id="offcanvasNavbarDropdown">
+                <NavDropdown.Item href="#action3">Chill</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">Moody</NavDropdown.Item>
+                <NavDropdown.Item href="#action5"> Ambient </NavDropdown.Item>
               </NavDropdown>
             </Nav>
             <Form className="d-flex">
               <FormControl
                 type="search"
-                placeholder="Search"
+                placeholder="Search for notes..."
                 className="me-2"
                 aria-label="Search"
 
