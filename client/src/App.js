655fe1c93c7e6d81c47c352b3a6c83984660a23a
Peter Song <psong562@gmnail.com>
2022-01-27 11:59:14 -0800

    added loading gif

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,38 @@ import {
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import TutorialModal from './components/TurtorialModal';
+import TutorialModal from './components/TutorialModal/TurtorialModal';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Lofi1 from './components/Lofi1';
 import Lofi1Button from './components/Lofi1Button/Lofi1Button';
 import Navigation from './components/Navigation'
 
+
+import { useState, useEffect } from 'react'
+import Preloader from "./components/preLoader/pre.js";
+import "./style.css";
+
+
 // import Home from './pages/Home'
 const App = () => {
+
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+
   return (
     <Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+
       <Navigation />
         <Lofi1Button onClick={Lofi1} />
       <TutorialModal />
@@ -29,6 +50,7 @@ const App = () => {
         <Route path='/login' element={<Login />} />
         {/* <Route path='/' element={<Home />} /> */}
       </Routes>
+      </div>
     </Router>
   )
 }
