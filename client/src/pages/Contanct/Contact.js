d686698c2726904b6fa0bce9e537780112374a79
email <abe.solis2015@gmail.com>
2022-01-28 14:34:05 -0800

    contact page just needs bio and gh pages

--- client/src/pages/Contanct/Contact.js
+++ client/src/pages/Contanct/Contact.js
@@ -1,101 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css'
 import Peter from './peter.png'
 import Abe from './abe.png'
 import Austin from './austin.png'
 import Nathan from './nathan.png'
 import Navigation from '../../components/Navigation'
-import { Container, Row, Col, Card,Button} from 'react-bootstrap'
+import { Container, Row, Col, Card } from 'react-bootstrap'
 import ButtonComponent from '../../components/ButtonCompnent/ButtonComponent';
 
 const Contact = () => {
+
   return (
     <>
-    <section className=''>
-      <Navigation />
+      <section className='landing'>
+        <Navigation />
         <Container fluid="md">
           <Row>
             {/* PETERS CARD  */}
-            <Col> 
-            <Card className="text-center pb-10">
-              <Card.Header>Featured</Card.Header>
-              <Card.Body>
-                <Card.Title>Peter Song</Card.Title>
+            <Col className="bottom">
+              <Card className="text-center bgCard" >
+
+                <Card.Body>
+                  <Card.Title>Peter Song</Card.Title>
                   <Card.Img variant="top" src={Peter} />
-                <Card.Text>
-                  With supporting text below as a natural lead-in to additional content.
-                </Card.Text>
-                <ButtonComponent name= {'GitHub'} />
-              </Card.Body>
-              <Card.Footer className="text-muted">2 days ago</Card.Footer>
-            </Card>
-            {/* NATHANS CARD */}
+                  <Card.Text>
+                    With supporting text below as a natural lead-in to additional content.
+                  </Card.Text>
+
+                </Card.Body>
+                <Card.Footer className="text-muted"> <ButtonComponent name={'GitHub'} /></Card.Footer>
+              </Card>
+              {/* NATHANS CARD */}
             </Col>
             <Col>
-            <Card className="text-center">
-              <Card.Header>Featured</Card.Header>
-              <Card.Body>
-                <Card.Title>Nathan </Card.Title>
+              <Card className="text-center bgCard">
+
+                <Card.Body>
+                  <Card.Title>Nathan Monetelli </Card.Title>
                   <Card.Img variant="top" src={Nathan} />
-                <Card.Text>
-                  With supporting text below as a natural lead-in to additional content.
-                </Card.Text>
-                  <ButtonComponent name={'GitHub'} />
-              </Card.Body>
-              <Card.Footer className="text-muted">2 days ago</Card.Footer>
-            </Card>
+                  <Card.Text>
+                    With supporting text below as a natural lead-in to additional content.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer className="text-muted"> <ButtonComponent name={'GitHub'} /></Card.Footer>
+              </Card>
             </Col>
           </Row>
           <Row>
             {/* ABES CARD  */}
-            <Col>
-            <Card className="text-center">
-              <Card.Header>Featured</Card.Header>
-              <Card.Body>
-                <Card.Title>Abraham Solis</Card.Title>
+            <Col className="bottom">
+              <Card className="text-center bgCard">
+
+                <Card.Body>
+                  <Card.Title>Abraham Solis</Card.Title>
                   <Card.Img variant="top" src={Abe} />
-                <Card.Text>
-                  With supporting text below as a natural lead-in to additional content.
-                </Card.Text>
-                  <ButtonComponent name={'GitHub'} />
-              </Card.Body>
-              <Card.Footer className="text-muted">2 days ago</Card.Footer>
-            </Card>
+                  <Card.Text>
+                    With supporting text below as a natural lead-in to additional content.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer className="text-muted"> <ButtonComponent name={'GitHub'} /></Card.Footer>
+              </Card>
             </Col>
             {/* AUSTINS CARD */}
-            <Col>
-            <Card className="text-center">
-              <Card.Header>Featured</Card.Header>
-              <Card.Body>
-                <Card.Title>Austin</Card.Title>
+            <Col >
+              <Card className="text-center bgCard" >
+
+                <Card.Body>
+                  <Card.Title>Austin</Card.Title>
                   <Card.Img variant="top" src={Austin} />
-                <Card.Text>
-                  With supporting text below as a natural lead-in to additional content.
-                </Card.Text>
-                  <ButtonComponent name={'GitHub'} />
-              </Card.Body>
-              <Card.Footer className="text-muted">2 days ago</Card.Footer>
-            </Card>
+                  <Card.Text>
+                    With supporting text below as a natural lead-in to additional content.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer className="text-muted"> <ButtonComponent name={'GitHub'} /></Card.Footer>
+              </Card>
             </Col>
           </Row>
           <Row>
-            <Col>
-              <Card className="text-center">
-                <Card.Header>Featured</Card.Header>
-                <Card.Body>
+            {/* KEVINS CARD */}
+            <Col md={3}> </Col>
+            <Col md={6} className='center'>
+              <Card className="text-center bgCard" >
+
+                <Card.Body >
                   <Card.Title>Kevin</Card.Title>
                   <Card.Img variant="top" src={Austin} />
                   <Card.Text>
                     With supporting text below as a natural lead-in to additional content.
                   </Card.Text>
-                  <ButtonComponent name={'GitHub'} />
+
                 </Card.Body>
-                <Card.Footer className="text-muted">2 days ago</Card.Footer>
+                <Card.Footer className="text-muted"> <ButtonComponent name={'GitHub'} /></Card.Footer>
               </Card>
             </Col>
+          <Col md={3}> </Col>
           </Row>
         </Container>
-    </section>
+      </section>
     </>
 
 
