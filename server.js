ac1f68276c9dd9bf41f2e5922f29161f9f4603e0
kelleyindahouse <91229799+kelleyindahouse@users.noreply.github.com>
2022-01-31 16:31:32 -0500

    changes

--- server.js
+++ server.js
@@ -35,14 +35,14 @@ app.use(require('./routes'))
 
 app.get('*', (req, res) => res.sendFile(join(__dirname, 'client', 'public', 'index.html')))
 
-// require('./db')
-//   .then(() => app.listen(process.env.PORT || 3001))
-//   .catch(err => console.log(err))
+require('./db')
+  .then(() => app.listen(process.env.PORT || 3001))
+  .catch(err => console.log(err))
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/lofinotes');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/lofinotes');
 
-const io = require("socket.io")(3001, {
+const io = require("socket.io")(8080, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"]
