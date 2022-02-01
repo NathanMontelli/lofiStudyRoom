8e7edbf32cd9822bea0a86db7fa68042c6e0665d
RAH <richardaustinhall@gmail.com>
2022-02-01 10:59:53 -0800

    adding notes badge to profile page and updated some formatting

---
+++ client/src/components/NotesBadge/NotesBadge.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState } from 'react'
+import { Form, Button, Alert, Badge, Card, Row, Col, Container } from 'react-bootstrap'
+import Navigation from "../Navigation"
+import username from "../../utils/UserAPI"
+
+// let username = 
+
+const NotesBadge = () => {
+  return (
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col></Col>
+        <Col>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Your Notes:</Card.Title>
+              <Card.Text>
+                Select Notes you wish to view. 
+              </Card.Text>
+              <Button variant="primary">Set profile picture</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  )
+}
+
+
+
+export default NotesBadge
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -3,7 +3,13 @@ import './Profile.css'
 import { Row, Container, Col } from 'react-bootstrap'
 import WelcomeBadge from "../../components/WelcomeBadge"
 import Navigation from "../../components/Navigation"
+import Rain from '../../components/backgroundSounds/Rain';
+import Fire from '../../components/backgroundSounds/Fire';
+import Cars from '../../components/backgroundSounds/Cars';
+import Waves from '../../components/backgroundSounds/Waves';
+import Bot from '../../components/Modals/botModal';
 import TextEditor from "../../components/TextEditor"
+import NotesBadge from "../../components/NotesBadge"
 
 const Profile = () => {
   return (
@@ -12,8 +18,35 @@ const Profile = () => {
         <Navigation />
         <Container>
           <Row>
-            <Col md="auto"><WelcomeBadge /> </Col>
+            <Col><WelcomeBadge /> </Col>
+            <Col><NotesBadge /></Col>
           </Row>
+            <Col md="auto"></Col>
+          <Row>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <Bot />
+            <br />
+            <br />
+            <br />
+            <Rain />
+            <br />
+            <br />
+            <Fire />
+            <br />
+            <br />
+            <Cars />
+            <br />
+            <br />
+            <Waves />
+            </Row>
         </Container>
       </section>
      
